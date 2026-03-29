f2a9872d7e156d6e25887b7ec7934bf7827f79df
Amro Meshref <amr.khamees@ejust.edu.eg>
2026-03-30 00:20:32 +0200

    update cognition graph routing logic

--- src/os_assistant/core/graphs/cognition/routing/logic.py
+++ src/os_assistant/core/graphs/cognition/routing/logic.py
@@ -3,11 +3,25 @@ from langgraph.graph import END
 from os_assistant.utils.logger import get_logger
 from os_assistant.core.settings import (
     QUERY_CLARIFICATION_NODE,
+    QUERY_CLASSIFICATION_NODE,
     CLARIFICATION_NODE_MAX_ATTEMPTS,
 )
 
 logger = get_logger(__name__)
 
+def route_query_after_starting(state: OSAssistantState) -> str:
+    """
+    Determines the next node to route the query to after the initial start node.
+
+    Args:
+        state (OSAssistantState): The current state of the OS Assistant.
+
+    Returns:
+        str: The name of the node to route the query to.
+    """
+    if state.query_clarification.is_clarification_needed:
+        return QUERY_CLARIFICATION_NODE
+    return QUERY_CLASSIFICATION_NODE
 
 def route_query_after_classification(state: OSAssistantState) -> str:
     """
