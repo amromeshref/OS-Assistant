6439e22ae5208d03c211dd4ae7fc9f064c820dd1
Amro Meshref <amr.khamees@ejust.edu.eg>
2026-04-02 21:00:36 +0200

    update execution graph routing logic

--- src/os_assistant/core/graphs/execution/routing/logic.py
+++ src/os_assistant/core/graphs/execution/routing/logic.py
@@ -1,3 +1,5 @@
+from flask import logging
+
 from os_assistant.core.states.os_assistant_state import OSAssistantState
 from os_assistant.utils.logger import get_logger
 from os_assistant.core.settings import (
@@ -11,16 +13,21 @@ logger = get_logger(__name__)
 
 def route_after_starting(state: OSAssistantState) -> str:
     """
+    Route after starting the graph based on user validation and query classification.
     """
-    #add logging
+    if state.user_validation.user_feedback_type == "rejected":
+        logger.info("User rejected the plan during validation, routing to final response node.")
+        return FINAL_RESPONSE_NODE
+    
     if state.query_classification.query_type == "information":
         return INFORMATION_NODE
+    
     return CODE_EXECUTION_NODE
 
 def route_after_code_execution(state: OSAssistantState) -> str:
     """
+    Route after code execution based on query classification.
     """
-    #add logging
     if state.query_classification.query_type == "command":
         return FINAL_RESPONSE_NODE
     return INFORMATION_NODE
