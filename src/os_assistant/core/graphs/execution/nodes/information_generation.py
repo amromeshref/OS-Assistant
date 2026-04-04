3148df46189bd426a8967a1c753a9b62743c5eb1
Amro Meshref <amr.khamees@ejust.edu.eg>
2026-04-04 22:09:29 +0200

    update information node

--- src/os_assistant/core/graphs/execution/nodes/information_generation.py
+++ src/os_assistant/core/graphs/execution/nodes/information_generation.py
@@ -7,6 +7,7 @@ from os_assistant.prompts.information_generation import (
 )
 from os_assistant.utils.logger import get_logger
 from os_assistant.core.models import LLMModel
+from os_assistant.utils.helper_functions import command_executions_to_str, information_responses_to_str
 
 logger = get_logger(__name__)
 
@@ -15,40 +16,35 @@ def information_generation_node(state: OSAssistantState) -> OSAssistantState:
     """ """
     logger.info("Starting information generation node.")
 
-    if len(state.planning.information_steps) == 0:
-        logger.info("No information query provided")
-        logger.info("Completed information generation node.")
-        return state
+    # if len(state.planning.information_steps) == 0:
+    #     logger.info("No information query provided")
+    #     logger.info("Completed information generation node.")
+    #     return state
 
     llm_model = LLMModel()
     sys_prompt = get_information_generation_sys_prompt()
 
-    for information_step in state.planning.information_steps:
-        information_response = InformationResponse()
-        information_response.query = information_step.description
-        human_message = f"""
+    information_response = InformationResponse()
+    information_response.query = state.execution_orchestrator.next_step.step_details.description
+
+    human_message = f"""
 User's Original Query: {state.finalized_enhanced_query}
-Information Query: {information_step.description}
+Information Query: {information_response.query}
+Previous Command Executions(if any): {command_executions_to_str(state.command_executions)}
+Previous Information Responses(if any): {information_responses_to_str(state.generated_information_responses)}
 """
-        if len(state.command_executions) != 0:
-            command_executions_str = ""
-            for command_execution in state.command_executions:
-                command_executions_str += command_execution.model_dump_json() + "\n"
+    
+    response: str = llm_model.generate_response(
+        human_message=human_message,
+        system_message=sys_prompt,
+        structured_output=None,
+    )
 
-            human_message += f"""
-Command Execution Details:
-{command_executions_str}
-"""
-        response: str = llm_model.generate_response(
-            human_message=human_message,
-            system_message=sys_prompt,
-            structured_output=None,
-        )
+    # TODO: Add parsing logic here
 
-        # TODO: Add parsing logic here
-
-        information_response.answer = response
-        state.generated_information_responses.append(information_response)
+    information_response.answer = response
+    state.generated_information_responses.append(information_response)
+    state.current_step_index += 1
 
     state.generated_information_responses_status = "completed"
     logger.info("Completed information generation node.")
