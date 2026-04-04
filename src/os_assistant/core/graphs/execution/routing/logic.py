86953d1198d21adc4681dca2ca0184a0edb4a029
Amro Meshref <amr.khamees@ejust.edu.eg>
2026-04-04 22:10:31 +0200

    update execution graph routing logic

--- src/os_assistant/core/graphs/execution/routing/logic.py
+++ src/os_assistant/core/graphs/execution/routing/logic.py
@@ -3,9 +3,10 @@ from flask import logging
 from os_assistant.core.states.os_assistant_state import OSAssistantState
 from os_assistant.utils.logger import get_logger
 from os_assistant.core.settings import (
-    CODE_EXECUTION_NODE,
     INFORMATION_NODE,
+    CODE_EXECUTION_NODE,
     FINAL_RESPONSE_NODE,
+    EXECUTION_ORCHESTRATOR_NODE,
 )
 
 logger = get_logger(__name__)
@@ -19,15 +20,25 @@ def route_after_starting(state: OSAssistantState) -> str:
         logger.info("User rejected the plan during validation, routing to final response node.")
         return FINAL_RESPONSE_NODE
     
-    if state.query_classification.query_type == "information":
-        return INFORMATION_NODE
-    
-    return CODE_EXECUTION_NODE
+    return EXECUTION_ORCHESTRATOR_NODE
 
-def route_after_code_execution(state: OSAssistantState) -> str:
+def route_after_orchestrator(state: OSAssistantState) -> str:
     """
-    Route after code execution based on query classification.
+    Route after execution orchestrator node based on the next step determined by the orchestrator.
     """
-    if state.query_classification.query_type == "command":
-        return FINAL_RESPONSE_NODE
-    return INFORMATION_NODE
+    next_step_type = state.execution_orchestrator.next_step.step_type
+
+    if next_step_type == "command":
+        return CODE_EXECUTION_NODE
+    elif next_step_type == "information":
+        return INFORMATION_NODE
+
+def route_to_final_response(state: OSAssistantState) -> str:
+    """
+    Route to final response node after code execution or information generation based on the completion of all steps.
+    """
+    if state.current_step_index < state.total_steps:
+        return EXECUTION_ORCHESTRATOR_NODE
+    
+    return FINAL_RESPONSE_NODE
+
