34b57116e80a414c7a460ca894a4bf0bf175888c
Amro Meshref <amr.khamees@ejust.edu.eg>
2026-04-04 22:09:18 +0200

    update code execution node

--- src/os_assistant/core/graphs/execution/nodes/code_execution.py
+++ src/os_assistant/core/graphs/execution/nodes/code_execution.py
@@ -8,40 +8,40 @@ logger = get_logger(__name__)
 
 def code_execution_node(state: OSAssistantState) -> OSAssistantState:
     """
+    Node responsible for executing code/commands as part of the execution graph.
     """
     logger.info("Starting code execution node.")
 
-    if(len(state.planning.command_steps) == 0):
-        logger.info("No code execution steps provided")
-        logger.info("Completed code execution node.")
-        return state
+    # if(len(state.planning.command_steps) == 0):
+    #     logger.info("No code execution steps provided")
+    #     logger.info("Completed code execution node.")
+    #     return state
 
     sys_prompt: str = get_code_execution_sys_prompt()
     llm_model = LLMModel()
-
-
     
-    for command_step in state.planning.command_steps:
-        # Command output after running
-        command_output = run_command(command_step.command)
+    command = state.execution_orchestrator.next_step.step_details.command
 
-        human_message =f"""
+    # Command output after running
+    command_output = run_command(command)
+
+    human_message =f"""
 User's original query: {state.finalized_enhanced_query}
-Command Execution Details(Before running the command):
-{command_step.model_dump_json()}
-
+Current command to execute: {command}
 Command Output(After running):
 {command_output}
 """
-        response: CommandExecution = llm_model.generate_response(
-            system_message=sys_prompt,
-            human_message=human_message,
-            structured_output=CommandExecution
-        )
+    
+    response: CommandExecution = llm_model.generate_response(
+        system_message=sys_prompt,
+        human_message=human_message,
+        structured_output=CommandExecution
+    )
 
-        # TODO: Add parsing logic here
+    # TODO: Add parsing logic here
 
-        state.command_executions.append(response)
+    state.command_executions.append(response)
+    state.current_step_index += 1
 
     state.command_execution_status = "completed"
     logger.info("Completed code execution node.")
