bf6137a4011c4667e4ed534d758da64fa2ce9258
Amro Meshref <amr.khamees@ejust.edu.eg>
2026-04-19 00:36:18 +0200

    update execution graph routing logic

--- src/os_assistant/core/graphs/execution/routing/logic.py
+++ src/os_assistant/core/graphs/execution/routing/logic.py
@@ -7,7 +7,10 @@ from os_assistant.core.settings import (
     CODE_EXECUTION_NODE,
     FINAL_RESPONSE_NODE,
     EXECUTION_ORCHESTRATOR_NODE,
+    STEP_RESOLVER_NODE,
 )
+
+from os_assistant.utils.helper_functions import save_debug_state
 
 logger = get_logger(__name__)
 
@@ -20,7 +23,46 @@ def route_after_starting(state: OSAssistantState) -> str:
         logger.info("User rejected the plan during validation, routing to final response node.")
         return FINAL_RESPONSE_NODE
     
-    return EXECUTION_ORCHESTRATOR_NODE
+    logger.info("Executing first step of the plan.")
+
+    if state.planning.plan_steps[0].step_type == "command":
+        return CODE_EXECUTION_NODE
+    elif state.planning.plan_steps[0].step_type == "information":
+        return INFORMATION_NODE
+
+def router(state: OSAssistantState):
+    """
+    Main router function to determine the next node based on the current state of the OS Assistant.
+    """
+    save_debug_state(state, "Current state before routing.")
+
+    if state.steps_resolver_active:
+        current_resolving_step_index = state.current_resolving_step_index
+
+        logger.info(f"Routing to next resolved step with the index ({current_resolving_step_index}).")
+        next_resolved_step = state.steps_resolver[-1].resolved_steps[current_resolving_step_index]
+        if next_resolved_step.step_type == "command":
+            return CODE_EXECUTION_NODE
+        elif next_resolved_step.step_type == "information":
+            return INFORMATION_NODE
+            
+    total_steps = len(state.planning.plan_steps)
+
+    if state.current_step_index >= total_steps:
+        return FINAL_RESPONSE_NODE
+    
+    next_step = state.planning.plan_steps[state.current_step_index]
+    if next_step.dependencies_required:
+        logger.info(f"Next step ({state.current_step_index}) has dependencies, routing to step resolver.")
+        #save_debug_state(state, "Routing to step resolver due to dependencies.")
+        return STEP_RESOLVER_NODE
+    
+    logger.info(f"Routing to next step ({state.current_step_index}) without dependencies.")
+
+    if next_step.step_type == "command":
+        return CODE_EXECUTION_NODE
+    elif next_step.step_type == "information":
+        return INFORMATION_NODE
 
 def route_after_orchestrator(state: OSAssistantState) -> str:
     """
