1222b028547a4fcaa64e8a5db7303cb105e6f7c5
Amro Meshref <amr.khamees@ejust.edu.eg>
2026-04-24 15:08:21 +0300

    update settings.py

--- src/os_assistant/core/settings.py
+++ src/os_assistant/core/settings.py
@@ -14,10 +14,15 @@ CODE_EXECUTION_NODE = "code_execution_node"
 INFORMATION_NODE = "information_generation_node"
 FINAL_RESPONSE_NODE = "final_response_node"
 STEP_RESOLVER_NODE = "step_resolver_node"
+CODE_ERROR_HANDLING_NODE = "code_error_handling_node"
 
 # Configuration for the clarification node
 # shourd be added to config dir
 CLARIFICATION_NODE_MAX_ATTEMPTS = 10
+
+# Configuration for the command error handling node
+# should be added to config dir
+COMMAND_ERROR_HANDLING_MAX_ATTEMPTS = 5
 
 
 # Ollama settings
