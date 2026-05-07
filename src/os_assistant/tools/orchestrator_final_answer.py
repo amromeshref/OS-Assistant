8ac14022026720f4b37e6f42275ac5a8713181d9
Amro Meshref <amr.khamees@ejust.edu.eg>
2026-05-07 19:34:44 +0300

    remove unnecessary files

--- src/os_assistant/tools/orchestrator_final_answer.py
+++
@@ -1,7 +0,0 @@
-from langchain.tools import Tool
-
-orchestrator_final_answer_tool = Tool(
-    name="ReturnFinalStep",
-    func=lambda next_step: next_step,  # Identity function
-    description="Use this tool to return the final step at the end."
-)
